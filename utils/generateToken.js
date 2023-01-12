d0903cc0ad7db5e93a09e20c9fcf7658f34e6c37
jatin gautam <gautamjatin408@gmail.com>
2023-01-12 15:19:20 +0530

    initial commit

---
+++ utils/generateToken.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET_KEY,{
+    expiresIn: "7d",
+  });
+};
+export default generateToken;
